279f17057507e4ae3cb644e5667516949a598511
Diego Nunes <dgoonline1986@gmail.com>
2022-06-02 12:19:26 -0300

    first commit

---
+++ src/com/stefanini/aprendendocomeclipse/aula02/Aula02.java
@@ -0,0 +1,11 @@
+package com.stefanini.aprendendocomeclipse.aula02;
+
+public class Aula02 {
+	public static void main(String[] args) {
+		Aula02b funcionario= new Aula02b();
+		System.out.println(funcionario.cargo);
+		System.out.println(funcionario.idade);
+		System.out.println(funcionario.salario);
+	}
+
+}
---
+++ src/com/stefanini/aprendendocomeclipse/aula02/Aula02b.java
@@ -0,0 +1,10 @@
+package com.stefanini.aprendendocomeclipse.aula02;
+
+public class Aula02b {
+	public String cargo= "Desenvolvedor";
+	public int idade= 36;
+	public double salario= 3550;
+	
+	
+
+}
